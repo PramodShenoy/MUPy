8722d88a0ea47304ce1abdd69c5ad0257f4312fb
bhawesh96 <=>
2017-10-19 16:19:06 +0530

    Registration number> shared preference added

---
+++ app/src/main/java/com/example/gilgamesh/mupy/Logout.java
@@ -0,0 +1,25 @@
+package com.example.gilgamesh.mupy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Logout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logout);
+
+        Button logout = (Button) findViewById(R.id.logout);
+        logout = (Button) findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new RegistrationNumber.SessionManager(Logout.this).logout();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/gilgamesh/mupy/RegistrationNumber.java
@@ -0,0 +1,89 @@
+package com.example.gilgamesh.mupy;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegistrationNumber extends AppCompatActivity {
+
+    Button button_submit;
+    EditText editText_regno;
+    String EnteredRegNo;
+    SessionManager sessionManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_number);
+
+        button_submit = (Button) findViewById(R.id.submit);
+        editText_regno = (EditText) findViewById(R.id.RegNo);
+
+        sessionManager = new SessionManager(this);
+
+        if (sessionManager.isLoggedIn()) {
+                finish();
+                startActivity(new Intent(RegistrationNumber.this, MainActivity.class));
+            }
+
+            button_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EnteredRegNo = editText_regno.getText().toString();
+                if(RegNoCheck(EnteredRegNo)){
+                    sessionManager.createSession(EnteredRegNo);
+                    startActivity(new Intent(RegistrationNumber.this, MainActivity.class));
+                }
+            }
+        });
+    }
+
+//    validate the registration number
+    boolean RegNoCheck(String regNo) {
+//        length check
+        if(regNo.length() == 9) {
+            return true;
+        }
+        return false;
+    }
+
+    public static class SessionManager
+    {
+        private Context _context;
+        private SharedPreferences preferences;
+        private SharedPreferences.Editor editor;
+        public static final String PREF_NAME = "USER_INFO";
+        public static final String KEY_STATUS = "STATUS";
+
+        public static String REGNO;
+
+        public SessionManager(Context context)
+        {
+            this._context = context;
+            preferences = _context.getSharedPreferences(PREF_NAME, 0);
+            editor = preferences.edit();
+        }
+
+        public void createSession(String regno)
+        {
+            editor.putBoolean(KEY_STATUS, true);
+            editor.putString(REGNO, regno);
+            editor.commit();
+        }
+
+        public boolean isLoggedIn()
+        {
+            return preferences.getBoolean(KEY_STATUS, false);
+        }
+
+        public void logout()
+        {
+            editor.clear();
+            editor.commit();
+        }
+    }
+}
