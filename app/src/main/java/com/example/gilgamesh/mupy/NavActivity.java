1732a1c7fec30068c11dcda961af31362a7dc0cc
bhawesh96 <=>
2017-10-20 19:36:32 +0530

    async task for qr code

--- app/src/main/java/com/example/gilgamesh/mupy/NavActivity.java
+++ app/src/main/java/com/example/gilgamesh/mupy/NavActivity.java
@@ -36,6 +36,9 @@ public class NavActivity extends AppCompatActivity
         toggle.syncState();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        if (savedInstanceState == null) {
+            navigationView.getMenu().performIdentifierAction(R.id.speakers_show, 0);
+        }
     }
 
     @Override
@@ -43,24 +46,14 @@ public class NavActivity extends AppCompatActivity
                                            String permissions[], int[] grantResults) {
         switch (requestCode) {
             case 1: {
-
-                // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
                 } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
                     Toast.makeText(NavActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
                 }
                 return;
             }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
         }
     }
 
